fe3c8aee0c2dab7944972e0a44526f7750a701a9
naimulhassan <developernaimul00@gmail.com>
2024-08-22 16:02:27 +0600

    improve code

--- lib/view/screen/google_map_screen.dart
+++ lib/view/screen/google_map_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../controller/google_map_controller.dart';
 import '../component/google_map.dart';
